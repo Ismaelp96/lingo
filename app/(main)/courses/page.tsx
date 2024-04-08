791b0d1bdf16c553be6aa88e808b7c79cfa1f4dc
ismaelp96 <ismael6991@live.com>
2024-04-08 14:41:49 -0300

    User Progress #9

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,14 +1,23 @@
 import { ListCourses } from './list';
 
-import { getCourses } from '@/db/queries';
+import { getCourses, getUserProgress } from '@/db/queries';
 
 const CoursesPage = async () => {
-  const courses = await getCourses();
+  const userProgressData = getUserProgress();
+  const coursesData = getCourses();
+
+  const [courses, userProgress] = await Promise.all([
+    coursesData,
+    userProgressData,
+  ]);
 
   return (
     <div className='h-full max-w-[912px] px-3 mx-auto'>
       <h1 className='text-2xl font-bold text-neutral-700'>Language Courses</h1>
-      <ListCourses courses={courses} activeCourseId={1} />
+      <ListCourses
+        courses={courses}
+        activeCourseId={userProgress?.activeCourseId}
+      />
     </div>
   );
 };
