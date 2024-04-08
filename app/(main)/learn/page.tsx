34bcb7c33c82d217c683f5cad53b9139e27eac41
ismaelp96 <ismael6991@live.com>
2024-04-08 15:51:08 -0300

    Seed Script #10

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -15,19 +15,21 @@ const LearnPage = async () => {
   }
 
   return (
-    <div className='flex flex-row-reverse gap-[48px] px-6'>
-      <StickyWrapper>
-        <UserProgress
-          activeCourse={{ title: 'Spanish', imageSrc: '/es.svg' }}
-          hearts={5}
-          points={100}
-          hasActiveSubscription={false}
-        />
-      </StickyWrapper>
-      <FeedWrapper>
-        <Header title='Spanish' />
-      </FeedWrapper>
-    </div>
+    <>
+      <div className='flex flex-row-reverse gap-[48px] px-6'>
+        <StickyWrapper>
+          <UserProgress
+            activeCourse={userProgress.activeCourse}
+            hearts={userProgress.hearts}
+            points={userProgress.points}
+            hasActiveSubscription={false}
+          />
+        </StickyWrapper>
+        <FeedWrapper>
+          <Header title={userProgress.activeCourse.title} />
+        </FeedWrapper>
+      </div>
+    </>
   );
 };
 
